f2a49a8614d057a13555efea1978bac640afc52a
Emad Aljabry <ealjabry@gmail.com>
2024-10-30 00:43:02 -0400

    Creates new button on home page that when clicked takes you to a new create-post page

--- src/app/components/Body.js
+++ src/app/components/Body.js
@@ -3,12 +3,16 @@
 import '../stylesheets/Body.css'
 import SubmissionGrid from "./SubmissionGrid";
 import { submissions } from '../data';
+import Link from 'next/link';
 
 function Body() {
     
     return (
         <div className="body">
             <SubmissionGrid submissions ={submissions}></SubmissionGrid>
+            <Link href="/create-post">
+                <button className="create-post-button">+</button>
+            </Link>
         </div>
         
     )
---
+++ src/app/create-post/page.js
@@ -0,0 +1,15 @@
+"use client";
+
+import Header from "../components/Header";
+
+export default function CreatePost() {
+    return (
+        <div>
+            <Header />
+            <div className="create-post-container">
+                <h1>Create a New Post</h1>
+                {/* Add form elements here */}
+            </div>
+        </div>
+    );
+} 
