1886b2b666b73c7b628d4871a4918a080a0580d7
thinvanadium903 <ppb93083@uga.edu>
2024-11-21 10:00:57 -0500

    filled out about page, made an email address for support questions

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -9,19 +9,27 @@ export default function App() {
       <Header></Header>
       <h1 id='about-title'>About Us and This Project</h1>
       <p id='about-body'>
-        Hello! This project was created as part of a group project for UGA&apos;s Software Engineering 
-        course. We chose to design a pet blog as we all share a passion for pets and wanted a 
-        place for students and friends to share pet photos.
+        Hello! This project was created as part of a group project for UGA's Software Engineering 
+        course. We chose to design a pet blog as we all share a passion for pets, and wanted a 
+        place for students and friends to share their favorite photos.
       </p>
       <p id='about-body'>
         This site supports account creation, the ability to post and delete to the main 
-        dashboard, and the ability to show love to the posts you enjoy with a &apos;like&apos; button. 
+        dashboard, and the ability to show love to the posts you enjoy with a 'like' button. 
       </p>
-      <i id='about-disc'>
+      <p id='about-body'>Possible upcoming features for this site (but not guaranteed):</p>
+      <ul>
+        <li>Comments on posts with likes</li>
+        <li>Post editing</li>
+        <li>Sorting function</li>
+        <li>Search bar</li>
+      </ul>
+      <div id='about-disc'>
+        <i>
         Disclaimer: although we made best efforts to guarantee security and smoothness on this 
         site, performance is not guaranteed. You may encounter unexpected glitches. If you have a 
-        serious issue please contact us at </i> <a href='ppb93083@uga.edu' id='email'>ppb93083@uga.edu</a>
-      <i className='line'>.</i>
+        serious issue, please contact us at: </i><i id='email'>wheremydawgsathelp@gmail.com</i>
+      </div>
 
     </div>
   )
